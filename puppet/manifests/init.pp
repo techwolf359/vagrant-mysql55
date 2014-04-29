7ea1e91e056566cb9231431ac0a4a01d42a932ea
Jeremy Tinley <techwolf359@gmail.com>
2014-04-28 21:47:55 -0600

    Added Basic Puppet Provisioner
    Deploy a package to install the percona-release repository

---
+++ puppet/manifests/init.pp
@@ -0,0 +1,4 @@
+## Install percona-release package with provides the yum repository
+package { 'percona-release':
+	name => "http://www.percona.com/downloads/percona-release/percona-release-0.0-1.x86_64.rpm"
+}
